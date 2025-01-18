155b7cd70eefb1ca6a640202dba90f5e5abbb766
Артем Куцький <artemkampod4@gmail.com>
2025-01-18 18:36:11 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CinemaPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
