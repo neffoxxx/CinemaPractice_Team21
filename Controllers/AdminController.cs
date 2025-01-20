ded6c26588cc7bdfd490924b508283d56a972576
Артем Куцький <artemkampod4@gmail.com>
2025-01-20 20:23:16 +0200

    new controllers

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaPractice.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult ManageFilms() // todo
+        {
+            return View();
+        }
+        public IActionResult ManageSessions() // todo
+        {
+            return View();
+        }
+    }
+}
