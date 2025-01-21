4aaf9fb91cd5f88e00b42b0e187e6371b71a758b
Артем Куцький <artemkampod4@gmail.com>
2025-01-21 20:37:27 +0200

    fixed

---
+++ Infrastructure/Entities/Film.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Entities
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TrailerUrl { get; set; }
+        public string Genre { get; set; }
+        public string Cast { get; set; }
+        public double Rating { get; set; }
+    }
+
+}
---
+++ Infrastructure/Interfaces/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Interfaces
+{
+    public interface IEntity
+    {
+        public int Id { get; set; }
+    }
+}
